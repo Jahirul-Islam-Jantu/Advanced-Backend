2b9a3a9ee490c98c41567c742e15f42399a3e24e
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-10-30 21:21:41 +0600

    fibonacci api

--- src/app.js
+++ src/app.js
@@ -1,9 +1,33 @@
-const express = require("express")
-const fibonacci = (num) => {
+const express = require("express") // requiring express from installed express
+const app = express() // app created with express
+
+
+
+app.get("/api/get-fibonacci", (req, res)=>{ // we created an api with function on backend and make a get request with query params . we are taking the value from query string and calculate with the function of our backend.
+    const value = req.query.value // query params to get the value
+    res.json({ // our response to the request
+        actualValue:value,
+        result:fibonacci(value)
+    })
+})
+
+
+
+
+
+
+
+
+
+app.listen(5050,()=>{  // this is the listening port of our server
+    console.log("Server is listening on Port 5050")
+})
+
+const fibonacci = (num) => { // this is the backend function of our server
     if(num < 2){
         return num;
     } else {
         return (fibonacci(num - 1) + fibonacci(num - 2));
     }
 }
-console.log(fibonacci(25))
+console.log(fibonacci(5))
